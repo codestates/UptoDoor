21f60fd7504bc9f77b5f7e7e980f8bde07af1aa2
dydwns2441 <heoyjun6@gmail.com>
2021-09-10 17:42:02 +0900

    [feat]: dummydata

--- client/src/components/dummyData.js
+++ client/src/components/dummyData.js
@@ -1,15 +1,117 @@
-export const initialState = {
-  Users: {
-    id: 1,
-  },
-  Admins: {
-    id: 1,
-  },
-  Posts: {
-    id: 1
-  },
-  Orders: {
+export const initialUser = {
+  User: [
+    {
+      id: 1,
+      nickname: "용준이",
+      email: "test1@gmail.com",
+      mainAddress: "서울시 용산구 신흥로32길 4-33",
+      subAddress: "서울시 용산구 신흥로32길 4-44",
+      mobile: "010-7185-2791",
+      Order: [
+        {
+          id: 22,
+          order_time: "2021년 9월10일 03:00",
+          user_email: "test1@gmail.com",
+          user_mobile: "010-7185-2791",
+          store_id: "5",
+          totalPrice: "50000",
+        },
+        {
+          id: 23,
+          order_time: "2021년 9월11일 05:00",
+          user_email: "test1@gmail.com",
+          user_mobile: "010-7185-2791",
+          store_id: "9",
+          totalPrice: "14000",
+        },
+      ],
+    },
+    {
+      id: 2,
+      nickname: "쑥사장",
+      email: "test2@gmail.com",
+      mainAddress: "서울시 강서구 구구국구구111",
+      subAddress: "서울시 강서구 구구국구구111",
+      mobile: "010-6666-6666",
+      Store: {
+        id: 5,
+        name: "쑥카페",
+        image: [],
+        address: "서울시 강서구 구구국구구111",
+        introduce: "많이 놀러오세요~~띠요띵용",
+        category: "카페",
+        Menu: [],
+        //delivery_time: "",
+        //business_paper: "",
+      },
+    },
+  ],
+};
 
-  },
+export const initialCart = {
+  order_time: "2021년 9월12일 07:00",
+  user_email: "test1@gmail.com",
+  user_mobile: "010-7185-2791",
+  user_address: "서울시 용산구 신흥로32길 4-33",
+  store_id: "5",
+  Menu: [
+    {
+      id: 40,
+      name: "스페셜 스팸에그 토스트",
+      price: "4000",
+      detail: "스팸 + 에그 + 글루텐프리 식빵+ 특제 소스",
+      image: "",
+      quantity: "2",
+    },
+    {
+      id: 38,
+      name: "아메리카노",
+      price: "4000",
+      detail: "venti",
+      image: "",
+      quantity: "1",
+    },
+  ],
+  plus_check: true,
+  plus_detail: "우유 2개 추가, 아메리카노에 시럽 3번짜주세요",
+  plus_money: "2000",
+  delivery_term: "1개월",
+  delivery_day: "월요일, 화요일",
+  delivery_time: "오전 11시",
+  totalPrice: "112000",
+};
 
-}
+export const initialStore = {
+  id: 5,
+  name: "쑥카페",
+  image: [],
+  address: "서울시 강서구 구구국구구111",
+  introduce: "많이 놀러오세요~~띠요띵용",
+  category: "카페",
+  user_id: 2,
+  Menu: [
+    {
+      id: 38,
+      name: "아메리카노",
+      price: "4000",
+      detail: "venti",
+      image: "",
+    },
+    {
+      id: 39,
+      name: "카페라떼",
+      price: "5000",
+      detail: "venti size",
+      image: "",
+    },
+    {
+      id: 40,
+      name: "스페셜 스팸에그 토스트",
+      price: "4000",
+      detail: "스팸 + 에그 + 글루텐프리 식빵+ 특제 소스",
+      image: "",
+    },
+  ],
+  //delivery_time: "",
+  //business_paper: "",
+};

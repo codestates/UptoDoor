da08fe1cbf44d9fe6ee87b4f304010e55382e305
dydwns2441 <heoyjun6@gmail.com>
2021-09-11 03:34:32 +0900

    [fix]: navbar

--- client/src/components/common/NavBar/NavBar.js
+++ client/src/components/common/NavBar/NavBar.js
@@ -1,64 +1,59 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
 import {
   Header,
   Nav,
   ButtonWrapper,
   NavLogo,
-  IconWrapper,
+  ListLink,
+  NavWrapper,
+  MiddleButton,
+  IconButton,
+  SignupLink,
 } from "./StyledNavBar";
 
 function NavBar() {
+  // const { me } = useSelector((state) => state.user);
+
   return (
     <Header>
-      {/* 로고 */}
-      <NavLogo to="/">UptoDoor</NavLogo>
+      <NavWrapper>
+        {/* 로고 */}
+        <NavLogo to="/">UptoDoor</NavLogo>
 
-      {/* nav */}
-      <Nav className="sm-hidden">
-        <h2 className="visually-hidden">메뉴</h2>
-        <ul>
-          <li>
-            <Link to="/">구독찾기</Link>
-          </li>
-          <li>
-            <Link to="/">동네인증</Link>
-          </li>
-          <li>
-            <Link to="/">마이페이지</Link>
-          </li>
-        </ul>
-      </Nav>
-      <IconWrapper>
-        <button type="button" aria-label="알림 버튼">
-          <i className="far fa-bell"></i>
-        </button>
-        {/* 메뉴 버튼 */}
-        <button type="button" aria-label="메뉴 열기 버튼">
-          <i className="fas fa-bars"></i>
-        </button>
-      </IconWrapper>
-      <ButtonWrapper >
-        <button  type="button" aria-label="회원가입">
-          로그인
-        </button>
-        <button type="button" aria-label="회원가입">
-          회원가입
-        </button>
-      </ButtonWrapper>
-      {/* 알림 버튼 */}
+        {/* nav */}
+        <Nav>
+          <h2 className="visually-hidden">메뉴</h2>
+          <ul>
+            <li>
+              <ListLink to="/mapper">구독찾기</ListLink>
+            </li>
+            <li>
+              <ListLink to="/address">동네인증</ListLink>
+            </li>
+            <li>
+              <ListLink to="/mypage">마이페이지</ListLink>
+            </li>
+          </ul>
+        </Nav>
+      </NavWrapper>
+        <ButtonWrapper>
+          {/* 알림 버튼 */}
+          <IconButton type="button" aria-label="알림 버튼">
+            <i className="far fa-bell"></i>
+          </IconButton>
+          {/* 메뉴 버튼 */}
+          <IconButton type="button" aria-label="메뉴 열기 버튼">
+            <i className="fas fa-bars"></i>
+          </IconButton>
+          <MiddleButton type="button" aria-label="회원가입">
+            로그인
+          </MiddleButton>
+          <MiddleButton type="button" aria-label="회원가입">
+            <SignupLink to="/signup">회원가입</SignupLink>
+          </MiddleButton>
+        </ButtonWrapper>
+      
     </Header>
-
-    // <div>
-    //   <ul>
-    //     <Link to = '/'><li>홈</li></Link>
-    //     <li><Signin/></li>
-    //     <Link to = '/signup'><li>회원가입</li></Link>
-    //     <Link to = '/mypage'><li>마이페이지</li></Link>
-    //     <Link to = '/mapper'><li>[Map]우리동네 정기구독</li></Link>
-    //     <Link to = '/address'><li>[Address]동네 등록하기</li></Link>
-    //   </ul>
-    // </div>
   );
 }
 
--- client/src/components/common/NavBar/StyledNavBar.js
+++ client/src/components/common/NavBar/StyledNavBar.js
@@ -4,15 +4,15 @@ import {
   // BackgroundColor,
   MainColor,
   // PointColor,
-  // TextColor,
+  TextColor,
   // TextDarkGrey,
   // TextLightGrey,
 } from "../../GlobalStyle";
 import {
 //   SmallFont,
 //   BaseFont,
-//   MediumFont,
-  LargeFont,
+  MediumFont,
+  // LargeFont,
 //   UltraLargeFont,
 } from "../../GlobalStyle";
 
@@ -20,21 +20,62 @@ export const Header = styled.header`
   width: 100%;
   height: 50px;
   display: flex;
-  padding: 12px 12px;
+  padding: 15px 12px 0 12px;
   justify-content: space-between;
   margin-bottom: 30px;
+  transition: all 0.4s;
+
+  @media screen and (min-width: 767px) {
+    height: 80px;
+    margin: 0px auto;
+    padding: 15px;
+  }
+
+  @media screen and (min-width: 1120px) {
+    width: 1140px;
+    height: 80px;
+    margin: 0px auto;
+    padding: 15px;
+  }
+`;
+
+export const NavWrapper = styled.div`
+  display: flex;
 `;
 
 export const NavLogo = styled(Link)`
   color: ${MainColor};
   cursor: pointer;
-  font-size: ${LargeFont};
+  font-size: 28px;
   font-weight: 700;
   text-decoration: none;
   line-height: 1em;
+  margin-right: 12px;
+
+  @media screen and (min-width: 767px) {
+    font-size: 40px;
+    font-weight: 700;
+  }
+
+  @media screen and (min-width: 1120px) {
+    margin: 0 25px 0 0;
+  }
 `;
 
 export const Nav = styled.nav`
+  > ul {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    font-size: ${MediumFont};
+    font-weight: 500;
+    list-style: none;
+    cursor: pointer;
+    > li {
+      margin: 10px;
+    }
+  }
+
   > .visually-hidden {
     position: absolute !important;
     width: 1px !important;
@@ -52,28 +93,55 @@ export const Nav = styled.nav`
   }
 `;
 
-export const IconWrapper = styled.div`
-  > button {
-    width: 50px;
-    background: none;
-    border: none;
-    
-    > i {
-      font-size: 28px;
-      color: ${MainColor};
-      background: none;
-      border: none;
-    }
-
-    > img {
-      background : none;
-    }
-  }
+export const ListLink = styled(Link)`
+  color: ${TextColor};
+  text-decoration: none;
 `;
 
 export const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const IconButton = styled.button`
+  width: 30px;
+  background: none;
+  border: none;
+  cursor: pointer;
+  line-height: 1.2rem;
+  margin-right: 10px;
+
+  > i {
+    font-size: 26px;
+    color: ${MainColor};
+    background: none;
+    border: none;
+  }
+
+  @media screen and (min-width: 767px) {
+    display: none !important;
+  }
+`;
+
+export const MiddleButton = styled.button`
+  margin: 0 5px;
+  width: 90px;
+  height: 40px;
+  font-size: 18px;
+  font-weight: 500px;
+  background-color: ${MainColor};
+  color: #fff;
+  border: none;
+  border-radius: 10px;
+  text-align: center;
+
   @media screen and (max-width: 767px) {
     display: none !important;
   }
 `;
 
+export const SignupLink = styled(Link)`
+  text-decoration: none;
+  color: #fff;
+`;

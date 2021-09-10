b99c3f90413e5ac2fec62ed89e7531250aeeae88
이숙영 <22sook00@gmail.com>
2021-09-10 23:01:57 +0900

    [fix]: global style setting

--- client/src/components/GlobalStyle.js
+++ client/src/components/GlobalStyle.js
@@ -1,9 +1,28 @@
 import {createGlobalStyle} from 'styled-components'
 
+/* margin,padding 은 90% */
+
+/*color*/
+export const BackgroundColor = '#fff';
+export const MainColor = '#245CCE';
+export const PointColor = '#ff5954';
+export const TextColor = '#000';
+export const TextDarkGrey = '#434A54';
+export const TextLightGrey = '#656D78';
+
+/*font*/
+export const SmallFont = '14px';
+export const BaseFont = '16px';
+export const MediumFont = '20px';
+export const LargeFont = '24px';
+export const UltraLargeFont = '60px';
+
 export const GlobalStyle = createGlobalStyle`
   * {
     margin : 0px; 
     padding : 0px;
     box-sizing: border-box;
-  }
+    font-family: -apple-system, 'Noto Sans KR', sans-serif;
+  };
 `
+

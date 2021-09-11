3fcea10394223e50652410d7c9c7b3f74589a1a7
dydwns2441 <heoyjun6@gmail.com>
2021-09-11 17:44:28 +0900

    [feature]: 크기별 button 만들기

---
+++ client/src/components/common/Button/Button.js
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+import { MainColor, BackgroundColor } from "../../GlobalStyle";
+
+export const SmallButton = styled.button`
+  margin: 0 5px;
+  width: 90px;
+  height: 40px;
+  font-size: 18px;
+  font-weight: 500px;
+  border: 1px solid ${MainColor};
+  border-radius: 8px;
+  text-align: center;
+  color: ${({ primary }) => (primary ? BackgroundColor : MainColor)};
+  background-color: ${({ primary }) => (primary ? MainColor : BackgroundColor)};
+`;
+
+export const MiddleButton = styled.button`
+  width: 200px;
+  height: 40px;
+  font-size: 18px;
+  font-weight: 700px;
+  cursor: pointer;
+  padding: 8px;
+  text-align: center;
+  border-radius: 8px;
+  border: 1px solid ${MainColor};
+  color: ${({ primary }) => (primary ? BackgroundColor : MainColor)};
+  background-color: ${({ primary }) => (primary ? MainColor : BackgroundColor)};
+`;
+
+export const LagreButton = styled.button`
+  width: 280px;
+  height: 40px;
+  font-size: 18px;
+  font-weight: 700px;
+  cursor: pointer;
+  padding: 8px;
+  border-radius: 8px;
+  border: 1px solid ${MainColor};
+  text-align: center;
+  color: ${({ primary }) => (primary ? BackgroundColor : MainColor)};
+  background-color: ${({ primary }) => (primary ? MainColor : BackgroundColor)};
+`;

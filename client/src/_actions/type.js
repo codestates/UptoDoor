c37b775a72bc9858f13e439242d05cf3e50393a8
dydwns2441 <heoyjun6@gmail.com>
2021-09-14 17:38:51 +0900

    [feature]:usercart

--- client/src/_actions/type.js
+++ client/src/_actions/type.js
@@ -4,6 +4,9 @@ export const SIGNIN = 'SIGNIN';
 export const SIGNOUT = 'SIGNOUT';
 export const MYPAGE_USER = 'MYPAGE_USER';
 export const DELETE_USER = 'DELETE_USER';
+export const ADD_MAIN_ADDRESS = "ADD_MAIN_ADDRESS";
+export const ADD_SUB_ADDRESS = "ADD_SUB_ADDRESS";
+
 
 //admin signUp/signIn/signOut/mypage/deleteAdmin
 
@@ -14,3 +17,4 @@ export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
 export const REMOVE_FROM_All_CART = "REMOVE_FROM_All_CART";
 export const ADD_CART = "ADD_CART";
 export const ADD_ALL_CART_TO_ORDER = "ADD_ALL_CART_TO_ORDER";
+
--- client/src/_actions/user_action.js
+++ client/src/_actions/user_action.js
@@ -3,7 +3,7 @@ import {
   SIGNIN , 
   SIGNOUT , 
   MYPAGE_USER ,
-  DELETE_USER } from './type'
+  DELETE_USER,ADD_MAIN_ADDRESS,ADD_SUB_ADDRESS } from './type'
 
 // import axios from 'axios'
 // axios.defaults.withCredentials = true
@@ -23,13 +23,15 @@ export const signUp = (userinfo) => {
 
 //유저 signin post 요청
 export const signIn = (userinfo) => {
+  //axios코드 result
+
   return {
-    type : SIGNIN,
-    payload : {
-      userinfo
-    }
-  }
-}
+    type: SIGNIN,
+    payload: {
+      userinfo,
+    },
+  };
+};
 
 //유저 signout post 요청
 export const signOut = (userinfo) => {
@@ -58,3 +60,34 @@ export const deleteUser = (userinfo) => {
     }
   }
 }
+
+//main email 보내기
+export const addMainAddress = (mainAddress, mainAddressDetail) => {
+  const main = {
+    mainAddress,
+    mainAddressDetail,
+  };
+  console.log(main); 
+  return {
+    type: ADD_MAIN_ADDRESS,
+    payload: {
+      mainAddress,
+      mainAddressDetail
+    },
+  };
+};
+
+export const addSubAddress = (subAddress, subAddressDetail) => {
+  const sub = {
+    subAddress,
+    subAddressDetail,
+  };
+    console.log(sub)
+  return {
+    type: ADD_SUB_ADDRESS,
+    payload: {
+      subAddress,
+      subAddressDetail
+    },
+  };
+};

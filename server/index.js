e98cbe8d2773328c7fdffa491a1b7c1432ba7b74
xxrrang <xxrrang2@gmail.com>
2021-09-14 11:25:06 +0900

    [fix] https deploy redirect

--- server/index.js
+++ server/index.js
@@ -22,6 +22,16 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/health-check', (req, res) => {
+  res.send('health success');
+});
+
+app.use(function (req, res, next) {
+  if(!req.secure && req.get('X-Forwarded-Proto') !== 'https') {
+    res.redirect('https://' + req.get('Host') + req.url);
+  } else next();
+});
+
 app.listen(port, () => {
   console.log(`UptoDoor listening at http://localhost:${port}`);
 });

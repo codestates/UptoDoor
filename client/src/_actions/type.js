e3d8c38dca3c174b8d5433981718ea7bbf612c7c
dydwns2441 <heoyjun6@gmail.com>
2021-09-14 11:02:24 +0900

    [feature]: usercart

--- client/src/_actions/type.js
+++ client/src/_actions/type.js
@@ -6,3 +6,11 @@ export const MYPAGE_USER = 'MYPAGE_USER';
 export const DELETE_USER = 'DELETE_USER';
 
 //admin signUp/signIn/signOut/mypage/deleteAdmin
+
+
+//cart action
+export const SET_QUANTITY = "SET_QUANTITY";
+export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
+export const REMOVE_FROM_All_CART = "REMOVE_FROM_All_CART";
+export const ADD_CART = "ADD_CART";
+export const ADD_ALL_CART_TO_ORDER = "ADD_ALL_CART_TO_ORDER";

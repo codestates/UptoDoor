a3b9f60567f5fa1e242b81449bbab9bf248abf5f
xxrrang <xxrrang2@gmail.com>
2021-09-15 23:53:29 +0900

    [fix] fix to code

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 const port = 3060;
 
 app.use(cors({
-  origin: true,
+  origin: 'http://localhost:3000',
   credentials: true,
   methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
 }));

79b31bcf40bffedff65edf35d3c7c48d1774596d
xxrrang <xxrrang2@gmail.com>
2021-09-15 16:48:19 +0900

    [fix] jwt token

--- server/controllers/Users/Signup.js
+++ server/controllers/Users/Signup.js
@@ -1,4 +1,6 @@
 const { user } = require('../../models');
+const crypto = require('crypto');
+const { generateAccessToken, generateRefreshToken, sendAccessToken, sendRefreshToken } = require('../Tokenfunc/index');
 module.exports = async (req, res) => {
     //이메일을 받아와서 DB를 확인하여 이메일 인증이 되었는지 체크
     const Data = req.body
@@ -8,7 +10,8 @@ module.exports = async (req, res) => {
     if(checkData.emailcheck === 'false'){
         res.status(409).send({message: 'require email check'})
     }else{
-        await user.update({age:Data.age,gender:Data.gender,mobile:Data.mobile,nickname:Data.nickname,password:Data.password}, {where : {email : receiverEmail}})
+        const hashpassword = crypto.createHash('sha512').update(Data.password).digest('hex');
+        await user.update({age:Data.age,gender:Data.gender,mobile:Data.mobile,nickname:Data.nickname,password:hashpassword}, {where : {email : receiverEmail}})
         res.status(200).send({message: 'Signup success'});
     }
 }

9ae4fd062f4a77142638603ae4ea616a1b468c25
dydwns2441 <heoyjun6@gmail.com>
2021-09-19 02:13:41 +0900

    [feature]: footer

--- client/src/components/common/Footer/Footer.js
+++ client/src/components/common/Footer/Footer.js
@@ -1,11 +1,44 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import {
+  FooterContainer,
+  FooterWrapper,
+  AboutCompany,
+  AdminLink,
+} from "./StyledFooter";
 
 function Footer() {
   return (
-    <div>
-      footer
-    </div>
-  )
+    <FooterContainer>
+      <FooterWrapper>
+        <h5>고객센터 </h5>
+        <h2>02-1009-1009</h2>
+        <p>평일 09:00 ~ 18:00(주말 & 공휴일 휴무)</p>
+        <AboutCompany>
+          <p>회사명: EtemMerge | 서비스명: UptoDoor </p>
+        </AboutCompany>
+        <AdminLink>
+          <span>공동 대표: </span>
+          <a href="https://github.com/dydwns2441" target="_blank" rel="noreferrer">
+            허용준 GitHub |
+          </a>
+          <a href="https://github.com/22sook00.com" target="_blank" rel="noreferrer">
+            {" "}
+            이숙영 GitHub |
+          </a>
+          <a href="https://github.com/xxrang" target="_blank" rel="noreferrer">
+            {" "}
+            이재랑 GitHub |
+          </a>
+          <a href="https://github.com/Oh-Myeongjae" target="_blank" rel="noreferrer">
+            {" "}
+            오명재 GitHub
+          </a>
+        </AdminLink>
+        <small>BowWow © {new Date().getFullYear()} All rights reserved.</small>
+      </FooterWrapper>
+    </FooterContainer>
+  );
 }
 
 export default Footer

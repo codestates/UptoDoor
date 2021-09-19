74ab932b3904c8d0f2ebe72f092c57a4094210a0
xxrrang <xxrrang2@gmail.com>
2021-09-20 04:03:13 +0900

    [fix]: code fix

--- client/src/components/AdminPost/AdminUploadStore.tsx
+++ client/src/components/AdminPost/AdminUploadStore.tsx
@@ -12,6 +12,7 @@ import {
 import Slider,{Settings} from "react-slick";
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import axios from 'axios';
 
 import MapSelectModal from '../common/Modal/MapSelectModal'
 
@@ -30,36 +31,42 @@ function AdminUploadStore(
   const [openModal , setOpenModal] = useState(false);
   const [imgs , setImgs]:any = useState([]); 
   const dropHandler = (files:any) => {
-    // console.log('====',files);
-    // let formData = new FormData();
-    // const config = {
-    //   header : { 'content-type' : 'multipart/form-data'}
-    // }
-    // formData.append('file',files[0]);
-    // console.log('==fileconfig==',formData,config);
-
-    // //dispatch action axios 관리된거 와야함.
-    // axios.post('https://localhost:3001',formData,config)
-    // .then((res)=>{
-    //   if(res.data.success){
-    //     setImgs([...imgs,res.data.filePath])
-    //   }else{
-    //     alert('파일저장실패')
-    //   }
-    // })
-    // .catch((err)=>{
-    //   return console.log('==file 가져오기 실패===',err)
-    // })
+    
 
     if(imgs.length === 5){
       console.log('stop ,, stop ..')
       setOpenModal(true);
     }else{
-      setImgs([...imgs,files[0].path]);
-      props.updateFiles([...imgs,files[0].path])
-      //store_image 로 담을것.
-      // console.log('===img 경로보기===',imgs);
-    }
+      /* const reader = new FileReader();
+ 
+       //파일리더가 파일의 데이터를 url경로로 만들어준다. 때문에 src에 집어 넣어서 사용가능
+       reader.readAsDataURL(files[0]);
+ 
+       reader.onload = () => {
+         //파일리더가 파일을 정상적으로 렌더하면 성공상태가 2
+         if (reader.readyState === 2) {
+           setImgs([...imgs,reader.result]);
+         }
+        };*/
+     const formData = new FormData();
+     const config = {
+       headers: { 'content-type' : 'multipart/form-data'}
+     } 
+     formData.append('file',files[0]);
+     //dispatch action axios 관리된거 와야함.
+     axios.post('http://localhost:3060/image',formData,config)
+     .then((res)=>{
+       if(res.data.success){
+         setImgs([...imgs,res.data.filePath])
+       }else{
+         alert('파일저장실패')
+       }
+     })
+     .catch((err)=>{
+       return console.log('==file 가져오기 실패===',err)
+     })
+       props.updateFiles([...imgs,files[0].path])
+     }
   }
   const closeModal = () => {
     setOpenModal((prev)=>!prev)

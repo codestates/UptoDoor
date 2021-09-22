89efc93af11f3765d31700b82fc1aac4bad200af
이숙영 <22sook00@gmail.com>
2021-09-22 10:03:41 +0900

    [fix]: userorder/adminorder

--- client/src/components/Mypage/MyOrderList.tsx
+++ client/src/components/Mypage/MyOrderList.tsx
@@ -45,3 +45,74 @@ function MyOrderList({
 }
 
 export default MyOrderList
+
+{/* <MypageOrderListWrapper>
+<OrderListWrapper>
+<OrderListContent>
+  
+  <ListDate>
+    <p>다음 결제일: </p><h5>2021.10.17 </h5>
+    
+  </ListDate>
+  <ListInfo>
+    <DeliveryState blue>구독중</DeliveryState>
+    <img src="./images/toast.png" alt="order" />
+    <div>
+      <h4>남산아래</h4>
+      <p><span>내용:</span>스페셜 토스트, 아메리카노</p>
+      
+    </div>
+    
+  </ListInfo>
+  </OrderListContent>
+  <NextBtn type="button" onClick={()=>{moveDetailHandler("1")}}>
+    <i className="fas fa-angle-double-right"></i>
+  </NextBtn>
+</OrderListWrapper>
+
+<OrderListWrapper>
+<OrderListContent>
+  
+  <ListDate>
+    <p>다음 결제일: </p><h5>2021.10.17 </h5>
+    
+  </ListDate>
+  <ListInfo>
+    <DeliveryState blue>구독중</DeliveryState>
+    <img src="./images/toast.png" alt="order" />
+    <div>
+      <h4>남산아래</h4>
+      <p><span>내용:</span>스페셜 토스트, 아메리카노</p>
+      
+    </div>
+    
+  </ListInfo>
+  </OrderListContent>
+  <NextBtn type="button" onClick={()=>{moveDetailHandler("2")}}>
+    <i className="fas fa-angle-double-right"></i>
+  </NextBtn>
+</OrderListWrapper>
+<OrderListWrapper>
+<OrderListContent>
+  
+  <ListDate>
+    <p>다음 결제일: </p><h5>2021.10.17 </h5>
+    
+  </ListDate>
+  <ListInfo>
+    <DeliveryState >취소됨</DeliveryState>
+    <img src="./images/toast.png" alt="order" />
+    <div>
+      <h4>남산아래</h4>
+      <p><span>내용:</span>스페셜 토스트, 아메리카노</p>
+      
+    </div>
+    
+  </ListInfo>
+  </OrderListContent>
+  <NextBtn type="button" onClick={()=>{moveDetailHandler("3")}}>
+    <i className="fas fa-angle-double-right"></i>
+  </NextBtn>
+</OrderListWrapper>
+
+</MypageOrderListWrapper> */}

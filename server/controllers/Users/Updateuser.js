83db7d4d5998844efb3c57c250b9573e0289c480
xxrrang <xxrrang2@gmail.com>
2021-09-22 22:21:42 +0900

    [fix]: code fix

--- server/controllers/Users/Updateuser.js
+++ server/controllers/Users/Updateuser.js
@@ -3,14 +3,12 @@ const { checkAccess } = require('../Tokenfunc');
 const crypto = require('crypto');
 /* eslint-disable no-unused-vars */
 module.exports = async (req, res) => {
-    console.log('--------',req.body);
     const access = req.headers.cookie.split('accessToken=')[1].split(';')[0];
     const checkAccessToken = checkAccess(access);
     const { id } = checkAccessToken;
     const { password, name, mobile, gender, age } = req.body;
 
     try {
-        console.log('--- try -----');
         const hashpassword = crypto.createHash('sha512').update(password).digest('hex');
         await user.update({ password: hashpassword , name: name, mobile: mobile, gender: gender, age: age}, { where: { id: id }});
     }

f39183f1e4bbcda14984f6b75b30a8c921563768
xxrrang <xxrrang2@gmail.com>
2021-09-23 22:00:23 +0900

    [feat]: feat add to user mypage

--- server/controllers/Users/Userinfo.js
+++ server/controllers/Users/Userinfo.js
@@ -1,4 +1,9 @@
 /* eslint-disable no-unused-vars */
+const { user } = require('../../models');
+const { checkAccess } = require('../Tokenfunc');
 module.exports = async (req, res) => {
 
+    const access = req.headers.cookie.split('accessToken=')[1].split(';')[0];
+    const checkAccessToken = checkAccess(access);
+    const { id } = checkAccessToken;
 }

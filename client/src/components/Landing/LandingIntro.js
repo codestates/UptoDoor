ae794fbaac589e4d280af2bfd32bd24b2fdee424
이숙영 <22sook00@gmail.com>
2021-09-24 04:26:22 +0900

    [fix]: 랜딩페이지 메인 디자인

--- client/src/components/Landing/LandingIntro.js
+++ client/src/components/Landing/LandingIntro.js
@@ -2,24 +2,26 @@ import React from 'react'
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-// import Fade from "react-reveal/Fade"
+import Fade from "react-reveal/Fade"
 import {
   LandingIntroContainer,
   LandingIntroWrapper,
-  TextContainer,
+  Container,
+  FlexBox,
   ImgContainer,
-  FlexWrpper,
   SliderWrapper,
   CategoryTitleWrapper,
   CategoryTitle,
+  CategoryImgWrapper,
   CategoryImgs,
+  GradientEdge,
   IntroH1,
 } from './StyledLanding'
-import { categoryDummy, LandingImgDummy} from '../dummyData'
+import {category, categoryDummy} from '../dummyData'
 
 const LandingIntro = () => {
   const settings = {
-    dots: false,  // 슬라이드 밑에 점 보이게
+    // dots: true,  // 슬라이드 밑에 점 보이게
     arrows : false,
     lazyLoad : true,
     infinite: true,  // 무한으로 반복
@@ -30,63 +32,52 @@ const LandingIntro = () => {
     slidesToScroll: 1,  // 3장씩 뒤로 넘어가게
     centerMode: true,
     centerPadding: '0px', 
-    pauseOnHover : true,	// 슬라이드 이동	시 마우스 호버하면 슬라이더 멈추게 설정
+    // pauseOnHover : true,	// 슬라이드 이동	시 마우스 호버하면 슬라이더 멈추게 설정
 		vertical : true,
   };
   return (
     <LandingIntroContainer>
       <LandingIntroWrapper>
-        <FlexWrpper>
-          <IntroH1>우리동네 찾는 모든</IntroH1>
-          <TextContainer flexable id = 'text-container'>
+          <Container  id = 'container'>
+            <FlexBox>
+          <Fade top>
+            <IntroH1>우리동네 찾는 모든</IntroH1>
+          </Fade>
+            <Slider { ...settings }>
+            {category.map((el,idx)=>{
+              return (
+                <CategoryTitleWrapper key = {idx}
+                  className = 'category-title-wrapper'>
+                  <CategoryTitle> {el} </CategoryTitle>
+                </CategoryTitleWrapper>
+                )
+            })}
+            </Slider>  
+            </FlexBox>
+
+          <ImgContainer id = 'img-container'>
             <Slider { ...settings }>
               {categoryDummy.map((el,idx)=>{
                 return (
-                <SliderWrapper 
-                key = {idx} className='slider-wrapper'>
-                  <CategoryTitleWrapper className = 'category-title-wrapper'>
-                    <CategoryTitle>{el}</CategoryTitle>
-                  </CategoryTitleWrapper>
-
-                  {LandingImgDummy.map((img,idx)=>{
-                    return (
-                      <div
-                        key = {idx} className = 'category-img-wrapper'>
-                        {img.map((elements,idx)=>{
-                          return <CategoryImgs 
-                          key = {idx} 
-                          src = {elements.image} alt = 'imgs' />
-                        })}
-                      </div>
-                    )
-                  })}
-                </SliderWrapper>
-                )
-              })}
-            </Slider>   
-          </TextContainer>
-        </FlexWrpper>
-      
-        {/* <FlexWrpper>
-          <ImgContainer id = 'main-side-container'>
-            <Slider { ...settings }>
-              {LandingImgDummy.map((el,idx)=>{
-                return (
                 <SliderWrapper key = {idx} className='slider-wrapper'>
-                  <div className = 'category-img-wrapper'>
+                  <CategoryImgWrapper className = 'category-img-wrapper'>
                     {el.map((elements,idx)=>{
-                      return <CategoryImgs 
+                      return (
+                        <CategoryImgs 
                       key = {idx} 
                       src = {elements.image} alt = 'imgs' />
+                    )
                     })}
-                  </div>
+                  </CategoryImgWrapper>
                 </SliderWrapper>
                 )
               })}
             </Slider>   
           </ImgContainer>
-        </FlexWrpper> */}
 
+        </Container>
+        {/* 흐림효과 */}
+        <GradientEdge ></GradientEdge>
       </LandingIntroWrapper>
     </LandingIntroContainer>
   )

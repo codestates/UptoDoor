5eb786bf65efb12508d32c1eed9345ab09a20018
dydwns2441 <heoyjun6@gmail.com>
2021-09-24 20:45:35 +0900

    [fix]: 데이터편집

--- client/src/components/Mypage/MyOrderList.tsx
+++ client/src/components/Mypage/MyOrderList.tsx
@@ -8,48 +8,46 @@ import {
 
 function MyOrderList({ 
   moveDetailHandler , 
-  cart, orderDate}:any):any {
-
-
+  order, orderDate}:any):any {
+  console.log(order);
+  
   // console.log('selected_address:',cart.selected_address);
 
   return (
     <MypageOrderListWrapper>
-      {/* {cart.selected_address === '' ? 
-      <p>구독중인 스토어가 없습니다.</p>
-      : */}
+      {order.length === 0 ?
+        <p>구독중인 스토어가 없습니다.</p>
+        :
         <>
-        {cart.menu.map((el:any,idx:any)=>{
-          return (
-            <OrderListWrapper key = {idx}>
-              <OrderListContent>
-                <ListDate>
-                  <p>다음 결제일: </p>
-                  <h5>{orderDate(cart.delivery_term)}</h5>
-                </ListDate>
-                <ListInfo>
-                  {/* {cancle 된 상품이면 ? 
-                  <DeliveryState blue>취소됨</DeliveryState> 
-                  :
-                  <DeliveryState blue>구독중</DeliveryState>} */}
-                  <DeliveryState blue>구독중</DeliveryState>
-                  <img src ={el.image} alt="ordered-img" />
-                  <div>
-                    <h4>남산아래</h4>
-                    <p><span>내용:</span>{el.detail}</p>
-                  </div>
+          {order && order.map((el: any, idx: any) => {
+            return (
+              <OrderListWrapper key={idx}>
+                <OrderListContent>
+                  <ListDate>
+                    <p>다음 결제일: </p>
+                    <h5>{orderDate(el.delivery_term)}</h5>
+                  </ListDate>
+                  <ListInfo>
+                    <DeliveryState blue>{el.state === "order" ? "구독중" : "취소됨"}</DeliveryState>
+                    <img src={el.menu[0].menu.image} alt="ordered-img" />
+                    <div>
+                      <h4>{el.store.name}</h4>
+                      <p><span>내용:</span>{el.menu && el.menu.map((el: any) => {
+                        return `${el.menu.name}, `
+                      })}</p>
+                    </div>
 
-              <NextBtn type="button" onClick={()=>{moveDetailHandler("1")}}>
-                <i className="fas fa-angle-double-right"></i>
-              </NextBtn>
-                </ListInfo>
-              </OrderListContent>
+                    <NextBtn type="button" onClick={() => { moveDetailHandler("1") }}>
+                      <i className="fas fa-angle-double-right"></i>
+                    </NextBtn>
+                  </ListInfo>
+                </OrderListContent>
       
-            </OrderListWrapper>
-          )
-        })}
+              </OrderListWrapper>
+            )
+          })}
         </>
-      {/* } */}
+      }
     </MypageOrderListWrapper>
   )
 }
--- client/src/components/Mypage/MyProfile.tsx
+++ client/src/components/Mypage/MyProfile.tsx
@@ -17,11 +17,11 @@ import { useSelector } from "react-redux";
 import { END_POINTS } from '../../_actions/type';
 
 function MyProfile(): any {
-  
-
+  const [user,setUser]:any = useState('')
+  const [orderList,setOrderList] = useState('')
   const cart = useSelector((state:any) => state.cart);
-  const user = useSelector((state:any) => state.user);
-
+  // const user = useSelector((state:any) => state.user);
+  // console.log(user);
   const [filteredOrderId, setFilteredOrderId]=useState("")
 
   const moveDetailHandler = (id:any) => {
@@ -69,12 +69,23 @@ function MyProfile(): any {
       ${getFullDate.getDate()}` 
     } 
   }
-  orderDate(cart.deliver_term);
+  // orderDate(user.user_orders.order.order_deliveries[0].delivery_term);
 
   useEffect(() => {
     axios.get(`${END_POINTS}/users/userinfo`)
       .then((res) => {
-        console.log(res);
+        const order = res.data.userdata.user_orders.map((el:any) => {
+          const { delivery_day, delivery_term, delivery_time } = el.order.order_deliveries;
+          const { state, totalprice, order_menus, store, user_name, selected_address, selected_address_detail, selected_mobile, createdAt, id, delivery_detail } = el.order;
+          const detail = delivery_detail.split(",")
+          const final = {
+            id,state,user_name,totalprice,store,selected_address,selected_address_detail,selected_mobile,createdAt,delivery_detail:detail[0],plusMoney:detail[1],delivery_time,delivery_term,delivery_day,menu:order_menus, 
+          }
+          return final;
+        })
+        // console.log("미친짓",order)
+        setOrderList(order);
+        setUser(res.data.userdata);
       }).catch((err) => {
         console.log(err);
     })
@@ -92,9 +103,8 @@ function MyProfile(): any {
                 <p>{user.email}</p>
                 {
                 user.mainAddress === null || 
-                user.mainAddressDetail === null||
-                user.subAddress === null || 
-                user.subAddressDetail === null ? 
+                user.mainAddressDetail === null
+                ?
                 <p>동네인증이 필요합니다.</p>
                 :
                 <>
@@ -102,9 +112,23 @@ function MyProfile(): any {
                 <p>{user.mainAddressDetail}</p>
                 </>
                 }
+                {/* {
+                user.subAddress === null || 
+                user.subAddressDetail === null
+                ?
+                <p>동네인증이 필요합니다.</p>
+                :
+                <>
+                <p>{user.subAddress}</p>
+                <p>{user.msubAddressDetail}</p>
+                </>
+                } */}
               </MypageContent>
               <ButtonWrapper>
-                <button><Link to="/adminpost">가게 등록</Link></button>
+                {user.position === "1" ?
+                  (<button><Link to="/adminedit">가게 관리</Link></button>) :
+                  (<button><Link to="/adminpost">가게 등록</Link></button>)
+                }
                 <button><Link to="/mypageedit">프로필 수정</Link></button>
               </ButtonWrapper>
             </MypageProfileWrapper>
@@ -125,7 +149,7 @@ function MyProfile(): any {
             /> 
             : 
             <MyOrderList 
-            cart = {cart}
+            order={orderList}
             orderDate = {orderDate}
             moveDetailHandler={moveDetailHandler} 
             />}

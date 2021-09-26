7e69e231a547ab17ae602b0779831d5765004964
xxrrang <xxrrang2@gmail.com>
2021-09-26 21:44:40 +0900

    [fix]: include 모델 수정

--- server/controllers/Admin/Admininfo.js
+++ server/controllers/Admin/Admininfo.js
@@ -1,30 +1,24 @@
-const { user, user_order, order_menu, menu, store, order, order_delivery } = require('../../models');
+const { user, order_menu, menu, store, order, order_delivery } = require('../../models');
 const { checkAccess } = require('../Tokenfunc');
 /* eslint-disable no-unused-vars */
 module.exports = async (req, res) => {
-    // const access = req.headers.cookie.split('accessToken=')[1].split(';')[0];
-    // const userInfo = checkAccess(access);
-    // const { id } = userInfo;
+    const access = req.headers.cookie.split('accessToken=')[1].split(';')[0];
+    const userInfo = checkAccess(access);
+    const { id } = userInfo;
 
     try {
-        const userdata = await user.find({ where: { id : 91 },
-            attributes: { exclude: ['password','main_Xvalue','main_Yvalue','sub_Xvalue','sub_Yvalue',
-                                    'emailcheck','billingkey','login_type','oauth_token','createdAt','updatedAt']},
-                include : [{ model: store, 
-                    attributes: { exclude: ['xvalue','yvalue','Business_paper','updatedAt','createdAt']},
-                    include: [{ model: order,
-                        attributes : { exclude: ['updatedAt','store_id']},
-                         include: [{ model: order_delivery,
-                             attributes: { exclude : ['createdAt','updatedAt','id','order_id']},
-                            model: order_menu,
-                             attributes: { exclude : ['createdAt','updatedAt','id','menu_id','order_id']},
-                                include: [{ model : menu }],
-                             attributes: { exclude : ['id','createdAt','updatedAt']} }] 
-                    }]
+        const orderdata = await user.findOne({ where: { id : id },
+            attributes: { exclude: ['password','main_Xvalue','main_Yvalue','sub_Xvalue','sub_Yvalue','emailcheck','billingkey','login_type','oauth_token','createdAt','updatedAt']},
+                include : [{ model: store, attributes: { exclude: ['xvalue','yvalue','Business_paper','updatedAt','createdAt']},
+                    include: [{ model: order, attributes : { exclude: ['updatedAt','store_id']},
+                             include: [{ model: order_menu, attributes: { exclude : ['createdAt','updatedAt','id','menu_id','order_id']},
+                                include: [{ model : menu, attributes: { exclude : ['id','createdAt','updatedAt']}}]
+                             }, { model: order_delivery , attributes: { exclude : ['order_id','id','createdAt','updatedAt']}}]
+                            }]
                     }],
-        })
+            })
         
-        res.status(200).send({ message: 'ok', userdata });
+        res.status(200).send({ message: 'ok', orderdata });
     }
     catch(err){
         console.log('--- admin my page ---',err);

a6f119ba050bb3a4ec05d944ce484d250ff84fd5
xxrrang <xxrrang2@gmail.com>
2021-10-03 03:08:13 +0900

    [fix]: 결제일 코드 수정

--- server/controllers/Payment/feedback.js
+++ server/controllers/Payment/feedback.js
@@ -7,15 +7,21 @@ module.exports = async (req, res) => {
     const count1 = await order_delivery.findOne({ where : { order_id: req.body.order_id }})
     
     const day = count1.payday.split('.')
-    const date = new Date(day[0], day[1], day[2]);
-    date.setDate(date.getDate()+28);
+    console.log('---year---',day[0])
+    console.log('---month---',day[1])
+    console.log('---day---',day[2])
+    const date = new Date(day[0], day[1]+1, day[2]);
+    console('--- date ----',date)
+    const date1 = date.getDate()+28
+    console('-- date1 --', date1)
+    date.setDate(date1);
+    console('--- dateset ----',date)
     const newYear = date.getFullYear();
     const newMonth = date.getMonth();
     const newDay = date.getDay();
     const nextPayDay = `${newYear}.${newMonth}.${newDay}`
-
-    await order_delivery.update({ paycount: sequelize.literal('paycount + 1')},{ where: { order_id : req.body.order_id}}, 
-    { payday: nextPayDay })
+    console('--- next ----',nextPayDay)
+    await order_delivery.update({ paycount: sequelize.literal('paycount + 1'), payday: nextPayDay },{ where: { order_id : req.body.order_id}})
     const count2 = await order_delivery.findOne({ where : { order_id: req.body.order_id }})
     
     if(count2.paycount < Number(count2.delivery_term)){

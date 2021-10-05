1b7b2aa4f4fb810f2ad03ce7e71de3d46640124f
xxrrang <xxrrang2@gmail.com>
2021-10-05 18:15:25 +0900

    [fix]: shell script 코드 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,6 +5,7 @@ export DATABASE_USER=$(aws ssm get-parameters --region ap-northeast-2 --names DA
 export DATABASE_PASSWORD=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PASSWORD --query Parameters[0].Value | sed 's/"//g')
 export DATABASE_PORT=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PORT --query Parameters[0].Value | sed 's/"//g')
 export DATABASE_HOST=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_HOST --query Parameters[0].Value | sed 's/"//g')
+export DATABASE=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE --query Parameters[0].Value | sed 's/"//g')
 export ACCESS_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names ACCESS_SECRET --query Parameters[0].Value | sed 's/"//g')
 export REFRESH_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names REFRESH_SECRET --query Parameters[0].Value | sed 's/"//g')
 export accessKeyId=$(aws ssm get-parameters --region ap-northeast-2 --names accessKeyId --query Parameters[0].Value | sed 's/"//g')
